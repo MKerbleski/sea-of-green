f9fd7fb53245976026c5a9e8825fb5037fffff43
mkerbleski <mkerbles@gmail.com>
2019-09-22 11:00:18 -0400

    test

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const { exec } = require('child_process')
 
-router.get('/sample', (req, res, next) => {
+const cb = (err, stdout, stderr) => {
+    if(err){
+        console.log('err', err)
+        console.log('stderr', stderr)
+    } else {
+        console.log('success')
+        console.log('\n------\nstdout', stdout)
+    }
+}
+
+const git = (command) => {
+    console.log('running ->', command)
+    return new Promise((resolve, reject) => {
+        exec(command, (err, stdout, stderr) => {
+            if(err){
+                console.log('err', err)
+                console.log('stderr', stderr)
+                reject()
+            } else {
+                console.log('\n------stdout', stdout)
+                resolve()
+            }
+        })
+    })
+}
+
+router.get('/git', async (req, res, next) => {
+    console.log('git')
+    await git('git status')
+    console.log('===========next===========')
+    await git('git add .')
+    console.log('===========next===========')
+    await git('git add .')
+    // await git('git reset HEAD')
 })
 
 module.exports = router;
